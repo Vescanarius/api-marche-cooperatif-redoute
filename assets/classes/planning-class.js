5688e5610ad3ce86b33986257e16d526dfb4263f
Thomas <gudinthomas@gmail.com>
2019-05-04 20:07:06 -0400

    planing sur 2 mois

--- assets/classes/planning-class.js
+++ assets/classes/planning-class.js
@@ -8,23 +8,70 @@ module.exports = (_db, _config) => {
 
 let Planning = class {
 
+    static getAllTeams() {
+        let equipes = ['caisse', 'caisseO', 'acc', 'coord1', 'coord2', 'demont', 'mont', 'prod']
 
-    static getTwoMonths(max) {
+        return equipes
+    }
+
+
+    static getTwoMonths() {
+
         return new Promise((next) => {
 
-            if (max != undefined && max > 0) {
-                db.query('SELECT * FROM members LIMIT 0, ?', [parseInt(max)])
-                    .then((result) => next(result))
-                    .catch((err) => next(err))
-            } else if (max != undefined) {
-                next(new Error(config.errors.wrongmaxValue))
-            } else {
-                db.query('SELECT * FROM members')
-                    .then((result) => next(result))
-                    .catch((err) => next(err))
-            }
+            let equipes = this.getAllTeams()
+            let queries = equipes.map((equipe) => {
+                return new Promise((next) => {
+                    db.query('SELECT * FROM members WHERE equipe=? ', [equipe])
+                        .then((result) => {
+
+                            let teamArray = result
+                            let today = new Date
+
+                            let queries = teamArray.map((consomacteur) => {
+                                return new Promise((resolve, reject) => {
+                                    db.query("SELECT COUNT(*) as services FROM services WHERE `member`=?", [consomacteur.id])
+                                        .then((result) => {
+                                            let nbServices = result[0].services
+                                            console.log(result)
+                                            let dateArrivee = new Date(consomacteur.dateAjout)
+                                            let nbDeMarches = Math.round((today - dateArrivee) / (7 * 24 * 60 * 60 * 1000))
+                                            // on crée la note pour chaque consomacteur
+                                            let note = nbServices / nbDeMarches
+
+                                            result = [consomacteur.name, consomacteur.prenom, consomacteur.tel, consomacteur.email, note]
+
+                                            resolve(result)
+                                        })
+                                        .catch((err) => next(err))
+                                })
+                            })
+
+                            Promise.all(queries)
+                                .then(results => {
+                                    //console.log(results)
+                                    next(results)
+                                })
+
+                        })
+                        .catch((err) => next(err))
+
+                })
+
+            })
+
+            Promise.all(queries)
+                .then(results => {
+                    //console.log(results)
+                    next(results)
+                })
+
         })
+
     }
+
+
+
 
 
 
