79c470127776a650f27b96a7cb0742527eddf474
Thomas <gudinthomas@gmail.com>
2019-05-04 22:53:21 -0400

    Finalisation de la classe Plannings

--- assets/classes/planning-class.js
+++ assets/classes/planning-class.js
@@ -1,4 +1,7 @@
 let db, config
+const { sortArray } = require('../functions');
+
+
 
 module.exports = (_db, _config) => {
     db = _db
@@ -9,9 +12,13 @@ module.exports = (_db, _config) => {
 let Planning = class {
 
     static getAllTeams() {
-        let equipes = ['caisse', 'caisseO', 'acc', 'coord1', 'coord2', 'demont', 'mont', 'prod']
-
-        return equipes
+        return new Promise((next) => {
+            db.query("SELECT * FROM teams")
+                .then((equipes) => {
+                    next(equipes)
+                })
+                .catch((err) => next(err))
+        })
     }
 
 
@@ -20,59 +27,81 @@ let Planning = class {
         return new Promise((next) => {
 
             let equipes = this.getAllTeams()
-            let queries = equipes.map((equipe) => {
-                return new Promise((next) => {
-                    db.query('SELECT * FROM members WHERE equipe=? ', [equipe])
-                        .then((result) => {
+                .then((equipes) => {
+                    // console.log(equipes)
 
-                            let teamArray = result
-                            let today = new Date
+                    let queries = equipes.map((equipe) => {
+                        //console.log(equipe)
+                        return new Promise((next) => {
+                            db.query('SELECT * FROM members WHERE equipe=? ', [equipe.id])
+                                .then((result) => {
 
-                            let queries = teamArray.map((consomacteur) => {
-                                return new Promise((resolve, reject) => {
-                                    db.query("SELECT COUNT(*) as services FROM services WHERE `member`=?", [consomacteur.id])
-                                        .then((result) => {
-                                            let nbServices = result[0].services
-                                            console.log(result)
-                                            let dateArrivee = new Date(consomacteur.dateAjout)
-                                            let nbDeMarches = Math.round((today - dateArrivee) / (7 * 24 * 60 * 60 * 1000))
-                                            // on crée la note pour chaque consomacteur
-                                            let note = nbServices / nbDeMarches
+                                    let teamArray = result
+                                    let today = new Date
 
-                                            result = [consomacteur.name, consomacteur.prenom, consomacteur.tel, consomacteur.email, note]
+                                    let queries = teamArray.map((consomacteur) => {
+                                        return new Promise((resolve, reject) => {
+                                            db.query("SELECT COUNT(*) as services FROM services WHERE `member`=?", [consomacteur.id])
+                                                .then((result) => {
+                                                    let nbServices = result[0].services
+                                                    let dateArrivee = new Date(consomacteur.dateAjout)
 
-                                            resolve(result)
+                                                    if (dateArrivee == null || dateArrivee == 'Invalid Date') {
+                                                        dateArrivee = new Date();
+                                                        dateArrivee.setDate(today.getDate() - 30);
+                                                    }
+
+                                                    let nbDeMarches = Math.round((today - dateArrivee) / (7 * 24 * 60 * 60 * 1000))
+
+                                                    // on crée la note pour chaque consomacteur
+                                                    let note = parseInt(nbServices) / parseInt(nbDeMarches)
+
+                                                    //console.log(note)
+
+                                                    result = {
+                                                        "nom": consomacteur.name,
+                                                        "prenom": consomacteur.prenom,
+                                                        "tel": consomacteur.tel,
+                                                        "email": consomacteur.email,
+                                                        "note": note
+                                                    }
+
+                                                    resolve(result)
+                                                })
+                                                .catch((err) => next(err))
                                         })
-                                        .catch((err) => next(err))
-                                })
-                            })
+                                    })
 
-                            Promise.all(queries)
-                                .then(results => {
-                                    //console.log(results)
-                                    next(results)
-                                })
+                                    Promise.all(queries)
+                                        .then(results => {
 
+                                            // On trie les résultats en fonction de la note
+                                            results.sort(sortArray("note"));
+
+                                            // On prends que les deux notes les plus basses
+                                            let max = equipe.nbPlanning
+
+                                            results = results.slice(0,max)
+                                            // Rangement dans un tableau
+                                            results = {
+                                                "equipe": equipe.nomComplet,
+                                                "equipeAbrege": equipe.abrege,
+                                                "membres": results
+                                            }
+
+                                            next(results)
+                                        })
+                                })
+                                .catch((err) => next(err))
                         })
-                        .catch((err) => next(err))
-
+                    })
+                    Promise.all(queries)
+                        .then(results => {
+                            //console.log(results)
+                            next(results)
+                        })
                 })
-
-            })
-
-            Promise.all(queries)
-                .then(results => {
-                    //console.log(results)
-                    next(results)
-                })
-
+                .catch((err) => next(err))
         })
-
     }
-
-
-
-
-
-
 }
