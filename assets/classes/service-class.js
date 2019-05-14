f988911c505c61661eee8604e6fb2639a4496b54
Thomas <gudinthomas@gmail.com>
2019-05-14 17:30:52 -0400

    debug delete service

--- assets/classes/service-class.js
+++ assets/classes/service-class.js
@@ -41,7 +41,7 @@ let Service = class {
     }
     static removeAllPlanning(date) {
         return new Promise((next) => {
-            db.query("DELETE FROM services WHERE `date`=? `source` LIKE 'planning' AND `status` LIKE 'pending'",[date])
+            db.query("DELETE FROM services WHERE `date` LIKE ? AND `source` LIKE 'planning' AND `status` LIKE 'pending'",[date])
                 .then((results) => next(true))
                 .catch((err) => next(err))
         })
